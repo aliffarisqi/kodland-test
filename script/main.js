0d434283f7205a7569ef3c00478388ceb548eb3a
bayualiffarisqi <jakartabanyu@gmail.com>
2022-11-22 05:13:35 +0700

    landing page untuk kodland test

---
+++ script/main.js
@@ -0,0 +1,79 @@
+const humburgur = document.querySelector(".humberger__btn")
+humburgur.onclick = function () {
+	navBar = document.querySelector(".navbar__link")
+	navBar.classList.toggle("show")
+}
+
+function goTentang() {
+	document.getElementById("tentang").scrollIntoView()
+}
+function goAturan() {
+	document.getElementById("aturan").scrollIntoView()
+}
+function goMembeli() {
+	document.getElementById("membeli").scrollIntoView()
+}
+function goHome() {
+	document.getElementById("home").scrollIntoView()
+}
+
+const allNav = document.querySelectorAll(".navbar-link")
+allNav.forEach((item) => {
+	const a = item.parentElement.querySelector("li:first-child")
+	a.addEventListener("click", function (e) {
+		e.preventDefault()
+
+		if (!this.classList.contains("active")) {
+			allNav.forEach((i) => {
+				const aLink = i.parentElement.querySelector("li:first-child")
+
+				aLink.classList.remove("active")
+			})
+		}
+
+		this.classList.toggle("active")
+	})
+})
+
+const Among = document.getElementById("modalDetailAmongUs")
+const Warni = document.getElementById("modalDetailWarni")
+const Warna = document.getElementById("modalDetailWarna")
+const spanAmong = document.getElementsByClassName("closeAmong")[0]
+const spanWarna = document.getElementsByClassName("closeWarna")[0]
+const spanWarni = document.getElementsByClassName("closeWarni")[0]
+
+function goDetailAmongUs() {
+	Among.style.display = "block"
+}
+spanAmong.onclick = function () {
+	Among.style.display = "none"
+}
+window.onclick = function (event) {
+	if (event.target == Among) {
+		Among.style.display = "none"
+	}
+}
+
+function goDetailWarna() {
+	Warna.style.display = "block"
+}
+spanWarna.onclick = function () {
+	Warna.style.display = "none"
+}
+window.onclick = function (event) {
+	if (event.target == Warna) {
+		Warna.style.display = "none"
+	}
+}
+
+function goDetailWarni() {
+	Warni.style.display = "block"
+}
+spanWarni.onclick = function () {
+	Warni.style.display = "none"
+}
+window.onclick = function (event) {
+	if (event.target == Warni) {
+		Warni.style.display = "none"
+	}
+}
